8fbd1c64a9c1c6e94111425de1b79849ee17a372
Nahuel <martinnahuelvazquez@gmail.com>
2022-06-12 19:30:56 -0300

    add deploy

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // ncluye la librería de express
 const express = require('express')
+const { palindrome } = require('./utils/for_testing')
 const app = express()
 
 // definimos un puerto para HTTP
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Creamos una ruta para el directorio raíz en este caso solo envía el texto 'Hello world!!!' pero es común que se envíe una vista (archivo HTML)
 app.get('/', (req, res) => {
-  res.send('Hello world a!!')
+  res.send(palindrome('shakira'))
 })
 
 // Comienza a escuchar el puerto definido 3000
