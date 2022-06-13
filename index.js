82d19d515e9e4f23a597ebfd64a318152bcb530b
Gabriela <gabriela-noely@hotmail.com>
2022-06-13 10:48:06 -0300

    Join with another computer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ncluye la librería de express
 const express = require('express')
-// const { palindrome } = require('./utils/for_testing')
+const { palindrome } = require('./utils/for_testing')
 const app = express()
 
 // definimos un puerto para HTTP
@@ -10,13 +10,13 @@ app.get('/health', (req, res) => {
   res.send('anda bien')
 })
 
-const palindrome = (string) => {
-  if (typeof string === 'undefined') return
-  return string
-    .split('') // convierte en un array
-    .reverse() // reversa el array
-    .join('') // convierte en un string
-}
+// const palindrome = (string) => {
+//   if (typeof string === 'undefined') return
+//   return string
+//     .split('') // convierte en un array
+//     .reverse() // reversa el array
+//     .join('') // convierte en un string
+// }
 
 // Creamos una ruta para el directorio raíz en este caso solo envía el texto 'Hello world!!!' pero es común que se envíe una vista (archivo HTML)
 app.get('/', (req, res) => {
@@ -28,6 +28,6 @@ app.listen(PORT, () => {
   console.log('Listen on the port 3000')
 })
 
-module.exports = {
-  palindrome
-}
+// module.exports = {
+//   palindrome
+// }
